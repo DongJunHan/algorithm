c2aba622cf2dc279acc28d713c9510a463d9e340
DongJunHan <gamedokdok@naver.com>
2023-04-21 14:48:57 +0900

    add comment

--- programmers/lv2/min_max.py
+++ programmers/lv2/min_max.py
@@ -2,6 +2,7 @@
 def solution(s):
     answer = ''
     arr = s.split(" ")
+    # a = list(map(int,s.split())) #str 타입 원소를 다른 타입으로 바꾸는 방법
     arr.sort(key=lambda x:int(x))
     answer = arr[0] + " " + arr[len(arr)-1]
     return answer
