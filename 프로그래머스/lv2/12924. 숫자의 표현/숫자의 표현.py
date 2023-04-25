69a95a7ec5edfea8d4398b83bda6c32201ad4a63
DongJunHan <47022968+DongJunHan@users.noreply.github.com>
2023-04-26 01:13:07 +0900

    [level 2] Title: 숫자의 표현, Time: 0.41 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,18 +1,3 @@
 def solution(n):
     answer = 0
-    i = 1
-    while True:
-        sum_n = 0
-        for j in range(i,n+1):
-            sum_n += j
-            if sum_n == n:
-                answer += 1
-                break
-            if sum_n > n:
-                break
-        
-        if i == n:
-            break
-        else:
-            i += 1
-    return answer
+    return len([i for i in range(1,n+1,2) if n % i == 0])
