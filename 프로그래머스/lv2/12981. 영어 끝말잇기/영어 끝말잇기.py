7b699a5ce70b6b986b9113b06b2335f47ebd1db3
DongJunHan <47022968+DongJunHan@users.noreply.github.com>
2023-05-04 17:03:57 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -3,11 +3,7 @@ def solution(n, words):
     answer = []
     tmp = {words[0]:0}
     for i in range(1,len(words)):
-        if words[i] in tmp.keys():
-            answer.append((i % n) + 1)
-            answer.append(math.ceil((i+1) / n))
-            return answer
-        if words[i][0] != words[i-1][len(words[i-1])-1]:
+        if words[i][0] != words[i-1][len(words[i-1])-1] or words[i] in tmp.keys():
             answer.append((i % n) + 1)
             answer.append(math.ceil((i+1) / n))
             return answer
