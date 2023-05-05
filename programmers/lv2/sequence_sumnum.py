ba876a29ddd6ad09a941d94cf347952bad73e39c
DongJunHan <gamedokdok@naver.com>
2023-05-05 11:41:23 +0900

    PR

---
+++ programmers/lv2/sequence_sumnum.py
@@ -0,0 +1,23 @@
+def solution(n):
+    answer = 0
+    i = 1
+    while True:
+        sum_n = 0
+        for j in range(i,n+1):
+            sum_n += j
+            if sum_n == n:
+                answer += 1
+                break
+            if sum_n > n:
+                break
+        if i == n:
+            break
+        else:
+            i += 1
+    return answer
+
+
+if __name__ == "__main__":
+    n = 100
+    ret = solution(n)
+    print(f"ret={ret}")
